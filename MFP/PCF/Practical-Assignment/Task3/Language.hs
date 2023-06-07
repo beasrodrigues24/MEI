a6e365fec954af1c657781adfc86bf3cd089481c
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2023-06-07 23:32:58 +0100

    PCF - PA-Task3 - Fix data type

--- MFP/PCF/Practical-Assignment/Task3/Language.hs
+++ MFP/PCF/Practical-Assignment/Task3/Language.hs
@@ -4,7 +4,7 @@ import LTerms
 import BTerms
 import Probability
 
-data ProgTerm = Asg Vars LTerm | EitherP (ProbRep, ProgTerm, ProgTerm) | Seq ProgTerm ProgTerm 
+data ProgTerm = Asg Vars LTerm | EitherP ProbRep ProgTerm ProgTerm | Seq ProgTerm ProgTerm 
         | Ife BTerm ProgTerm ProgTerm | Wh BTerm ProgTerm deriving Show
 
 -- Changes the value of a variable
@@ -22,7 +22,7 @@ apply xs q = mkD $ concatMap (\(m, p) -> multiply (wsem q m) p) (unD xs)
 
 wsem :: ProgTerm -> (Vars -> Double) -> Dist (Vars -> Double)
 wsem (Asg x t) m = return $ chMem x (sem t m) m 
-wsem (EitherP (pb, p, q)) m = do pD <- wsem p m ; qD <- wsem q m ; choose pb pD qD
+wsem (EitherP pb p q) m = do pD <- wsem p m ; qD <- wsem q m ; choose pb pD qD
 wsem (Seq p q) m = apply (wsem p m) q
 wsem (Ife b p q) m | bsem b m = wsem p m 
                    | otherwise = wsem q m
@@ -33,7 +33,7 @@ lexy = Leq x y
 xMaisy = (Asg X (Plus x y))
 yMaisUm = Asg Y (Plus y (Leaf (Right 1)))
 
-teste1 = EitherP (0.5, xMaisy, yMaisUm) -- D [(X=1;Y=1;Z=-100,0.5),(X=0;Y=2;Z=-100,0.5)]
+teste1 = EitherP 0.5 xMaisy yMaisUm -- D [(X=1;Y=1;Z=-100,0.5),(X=0;Y=2;Z=-100,0.5)]
 teste2 = Seq xMaisy yMaisUm -- D [(X=1,Y=2;Z=-100,1)]
 teste3 = Seq teste1 xMaisy -- D [(X=2;Y=1;Z=-100,0.5),(X=2;Y=2;Z=-100,0.5)]
 teste4 = Wh lexy (Seq xMaisy yMaisUm) -- D [(X=6;y=4;Z=-100,1.0)]

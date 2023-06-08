23818b375f274ac5e77f47c63500a9dc925e9a96
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2023-06-08 01:18:36 +0100

    PCF - PA Task 3 - Perfecting basic functionalities

--- MFP/PCF/Practical-Assignment/Task3/Language.hs
+++ MFP/PCF/Practical-Assignment/Task3/Language.hs
@@ -4,7 +4,7 @@ import LTerms
 import BTerms
 import Probability
 
-data ProgTerm = Asg Vars LTerm | EitherP ProbRep ProgTerm ProgTerm | Seq ProgTerm ProgTerm 
+data ProgTerm = Asg Vars LTerm | Choice ProbRep ProgTerm ProgTerm | Seq ProgTerm ProgTerm 
         | Ife BTerm ProgTerm ProgTerm | Wh BTerm ProgTerm deriving Show
 
 -- Changes the value of a variable
@@ -13,16 +13,18 @@ chMem x r m = \a -> if a /= x then m a else r
 
 -- Multiplies a whole dist by a probability
 multiply :: Dist (Vars -> Double) -> ProbRep -> [((Vars -> Double), ProbRep)]
-multiply l p = [(m, pi * p) | (m, pi) <- unD l]
+multiply (D l) p = [(m, pi * p) | (m, pi) <- l]
 -- Same thing: multiply (D ((m,pi):t)) p = mkD $ ([(m,pi*p)] ++ multiply t p)
 
+-- Applies a term q to all the possibilities from xs, updating the probability
 apply :: Dist (Vars -> Double) -> ProgTerm -> Dist (Vars -> Double)
-apply xs q = mkD $ concatMap (\(m, p) -> multiply (wsem q m) p) (unD xs)
+apply (D l) q = mkD $ concatMap (\(m, p) -> multiply (wsem q m) p) l
 -- Same thing: apply (D ((m,p):t)) q = mkD $ ((multiply (wsem q m) p) ++ apply t q)
 
+-- Evaluates a term
 wsem :: ProgTerm -> (Vars -> Double) -> Dist (Vars -> Double)
 wsem (Asg x t) m = return $ chMem x (sem t m) m 
-wsem (EitherP pb p q) m = do pD <- wsem p m ; qD <- wsem q m ; choose pb pD qD
+wsem (Choice pb p q) m = do pD <- wsem p m ; qD <- wsem q m ; choose pb pD qD
 wsem (Seq p q) m = apply (wsem p m) q
 wsem (Ife b p q) m | bsem b m = wsem p m 
                    | otherwise = wsem q m
@@ -33,7 +35,7 @@ lexy = Leq x y
 xMaisy = (Asg X (Plus x y))
 yMaisUm = Asg Y (Plus y (Leaf (Right 1)))
 
-teste1 = EitherP 0.5 xMaisy yMaisUm -- D [(X=1;Y=1;Z=-100,0.5),(X=0;Y=2;Z=-100,0.5)]
+teste1 = Choice 0.5 xMaisy yMaisUm -- D [(X=1;Y=1;Z=-100,0.5),(X=0;Y=2;Z=-100,0.5)]
 teste2 = Seq xMaisy yMaisUm -- D [(X=1,Y=2;Z=-100,1)]
 teste3 = Seq teste1 xMaisy -- D [(X=2;Y=1;Z=-100,0.5),(X=2;Y=2;Z=-100,0.5)]
 teste4 = Wh lexy (Seq xMaisy yMaisUm) -- D [(X=6;y=4;Z=-100,1.0)]
